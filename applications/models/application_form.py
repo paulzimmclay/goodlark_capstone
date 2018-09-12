52b7cadc52154861b294b1e152517f6139816089
pzc <paul.zimmerman.clayton@gmail.com>
2018-09-12 15:31:14 -0500

    refactoring to avoid duplicate class name

--- applications/models/application_form.py
+++ applications/models/application_form.py
@@ -4,5 +4,5 @@ from django.forms.widgets import HiddenInput
 
 
 class ApplicationForm(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     mailing_address = models.CharField(max_length=100)
--- applications/views/regular_application.py
+++ applications/views/regular_application.py
@@ -1,13 +1,27 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 from applications.forms import ApplicationUserForm, ApplicationForm
+from applications.models import ApplicationForm
 
 
 @login_required
 def regular_application(request):
-    # u = User.objects.get(id=request.user.id)
+    if request.method == 'POST':
+        u = User.objects.get(id=request.user.id)
+        application = ApplicationForm
+        application_user_form = ApplicationUserForm(request.POST, instance=request.user)
+        application_form = ApplicationForm(request.POST, instance=u.id)
+
+        if profile_form.is_valid() and user_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect('/')
+        else: 
+            print('forms not valid')
+
 
     application_user_form = ApplicationUserForm(initial={
         'first_name': request.user.first_name, 
