73faa2b3e433dc2dde671f3c3f5b2810b41e7a92
pzc <paul.zimmerman.clayton@gmail.com>
2018-09-17 15:42:49 -0500

    some admin progress, need to get user fields on application

--- applications/admin.py
+++ applications/admin.py
@@ -3,17 +3,29 @@ from .models.application_form import ApplicationFormModel
 from django.contrib.auth.models import User
 
 
+class ApplicationFormModelInline(admin.TabularInline):
+    model = ApplicationFormModel
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        ApplicationFormModelInline,
+    ]
+
+    fields = ('mailing_address')
 
 @admin.register(ApplicationFormModel)
 class ApplicationFormModelAdmin(admin.ModelAdmin):
 
+    # These functions allow the related object fields to be displayed on the list view
     def first_name(self, instance):
         return instance.user.first_name
 
     def last_name(self, instance):
         return instance.user.last_name
-
+    
+    # First and Last name are actually on the User Model 
     list_display = ('user', 'first_name', 'last_name', 'mailing_address',)
+    
 
 
 # class ApplicationInLine(admin.TabularInline):
--- goodlark_capstone/settings.py
+++ goodlark_capstone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'applications',
+    'import_export',
 ]
 
 MIDDLEWARE = [
