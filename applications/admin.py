feee232265e89ff950fde1616a7ab67b50ec81b4
pzc <paul.zimmerman.clayton@gmail.com>
2018-09-17 14:22:10 -0500

    anything

--- applications/admin.py
+++ applications/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models.application_form import ApplicationFormModel
 
-admin.site.register(ApplicationFormModel)
+# admin.site.register(ApplicationFormModel)
 
+@admin.register(ApplicationFormModel)
 class ApplicationFormModelAdmin(admin.ModelAdmin):
-      list_display = ['user','mailing_address']
+    list_display = ('user', 'mailing_address')
